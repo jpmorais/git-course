17fce2e4b2cb3191431a07c81d3be36256ec037a
jpmorais <jpmorais@gmail.com>
2023-02-25 13:03:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("node:http")
+
+
+const server = http.createServer((req, res) => {
+    res.write("Hello form node")
+    res.end()
+})
+
+
+server.listen(8000, () => {
+    console.log(`Server is listening on port 8000`)
+})
+
